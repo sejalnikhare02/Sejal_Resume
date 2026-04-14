4490f0962ba6a5284c8db1a38105cd7bf2bc79be
Praxnik <pratik.nikhare1712@gmail.com>
2026-04-14 15:12:42 +0530

    added CORS

--- portfolio-server/server.js
+++ portfolio-server/server.js
@@ -1,16 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const dbConfig = require("./src/config/dbConfig");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 const portfolioRoute = require("./src/routes/PortfolioRoute");
 
 app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 app.use("/uploads", express.static("uploads"));
+
 const port = process.env.PORT || 5000;
-// const port = 5000;
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
