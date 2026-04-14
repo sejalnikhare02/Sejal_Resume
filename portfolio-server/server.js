2d2efb08726f3423162c803979b7ff1b0b4db5c6
Praxnik <pratik.nikhare1712@gmail.com>
2026-04-14 14:57:53 +0530

    added Port

--- portfolio-server/server.js
+++ portfolio-server/server.js
@@ -9,9 +9,8 @@ const portfolioRoute = require("./src/routes/PortfolioRoute");
 app.use(express.json());
 app.use("/api/portfolio", portfolioRoute);
 app.use("/uploads", express.static("uploads"));
-// const port = process.env.PORT || 5000;
-const port = 5000;
-
+const port = process.env.PORT || 5000;
+// const port = 5000;
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
